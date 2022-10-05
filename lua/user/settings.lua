537dcf9db50babf1817d5ffa6f27179402001d54
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-10-05 09:59:02 +0530

    Whitespace settings modification: softtabstop, shiftwidth set to 4

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,22 +1,24 @@
---=============================================================================
+--=========================================================================
 -- Neovim Stable (version 0.8)
 -- Lua configuration: settings
---=============================================================================
+--=========================================================================
 local o = vim.opt
 local g = vim.g
 
 -- Whitespace
 o.expandtab = true
+o.softtabstop = 4
+o.shiftwidth = 4
 
 -- Indentation
 o.cindent = true
 o.breakindent = true
 
 -- Text dispaly area setup
-o.textwidth = 79 -- max line length
-o.wrapmargin = 0 -- chars to left of border when wrapping starts
-o.signcolumn = 'yes'
-o.colorcolumn = '+1'
+o.textwidth = 75 -- max line length
+o.wrapmargin = 1 -- chars to left of border when wrapping starts
+o.signcolumn = 'auto' -- hide signcolumn if there is nothing to show
+o.colorcolumn = '+0' -- highlight last legal column
 
 -- Show line numbers
 o.number = true
