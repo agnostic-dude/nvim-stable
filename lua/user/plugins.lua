ea8b9eee837e49e67603e9d77d173398688b7543
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-10-05 16:05:37 +0530

    Installed a bunch of plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -28,10 +28,39 @@ return packer.startup(function(use)
     -- Persistant, toggled floating terminals
     use "akinsho/toggleterm.nvim"
 
+  --> Utilities for coding
+  use "jiangmiao/auto-pairs" --> autocomplete & link parenthesis
+  use "tpope/vim-commentary" --> gc/gcc to comment/uncomment
+  use "lukas-reineke/indent-blankline.nvim" --> show indentation levels
+  use "RRethy/vim-illuminate" --> smart highlighting of words under cursor
+
+  --> Statusline
+  use { "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true } }
+
+  --> Git support
+  use { "lewis6991/gitsigns.nvim", requires = "nvim-lua/plenary.nvim" }
+
+  --> Treesitter
+  use { "nvim-treesitter/nvim-treesitter", run = "<Cmd>TSUpdate" }
+  use "nvim-treesitter/nvim-treesitter-textobjects"
+  use "p00f/nvim-ts-rainbow" --> colors matched parenthesis using treesitter
+
+
+  --> Cycle through all colorschemes
+  use "vim-scripts/CycleColor" --> cycle through colorschemes in rtp
+
+  --> Colorschemes
+  use "joshdick/onedark.vim" --> theme inspired by Atom
+  use "folke/tokyonight.nvim" --> TokyoNight colorscheme for VSCode
+  use "tomasiser/vim-code-dark" --> inspired by Dark+ scheme of VSCode
+  use "NLKNguyen/papercolor-theme" --> inspired by Google's Material Design
+  use "liuchengxu/space-vim-theme" --> dark & light with GUI support
 
     -- At end of all plugins to automatically setup configuration after
     -- clonning packer.nvim
     if packer_bootstrap then
+        print("Need to restart neovim after installation")
         packer.sync()
     end
 end)
