f578d0d483dc46c9143c32b881204372871efec3
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-11-23 10:20:39 +0530

    Added color config file

---
+++ lua/user/colors.lua
@@ -0,0 +1,7 @@
+--==============================================================================
+-- Neovim Stable Edtion (version 0.8)
+-- Lua configuration: color-settings
+--==============================================================================
+vim.g.termguicolors = true
+
+vim.cmd("colorscheme PaperColor")
