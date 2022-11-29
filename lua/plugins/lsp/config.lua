7dd191a8439e7bbd00d414a6bbb19d6c79b64e38
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-11-29 14:28:11 +0530

    Update: added nvim-cmp capabilities and rearranged file

--- lua/plugins/lsp/config.lua
+++ lua/plugins/lsp/config.lua
@@ -1,16 +1,36 @@
-local lsp_ok, lsp = pcall(require, "lspconfig")
-if not lsp_ok then
-  return
-end
-
+-- aliases
 local create_autocmd = vim.api.nvim_create_autocmd
 local create_augroup = vim.api.nvim_create_augroup
 
+-- if lspconfig is not installed exit neovim
+local lsp_ok, lspconfig = pcall(require, "lspconfig")
+if not lsp_ok then
+  error("lspconfig is not installed", 2) -- throw back to caller
+end
+
+-- base capabilities provided by lspconfig
+local capabilities = lspconfig.util.default_config.capabilities
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+-- if nvim-cmp is installed grab capabilities provided by it
+local cmp_ok, cmp_nvim = pcall(require, "cmp_nvim_lsp")
+if cmp_ok then
+  capabilities = vim.tbl_deep_extend("force",
+    capabilities, cmp_nvim.default_capabilities())
+else
+  print("need nvim-cmp to provide completion capabilities")
+end
+
+-- local capabilities = require("cmp_nvim_lsp").default_capabilities(
+--   vim.lsp.protocol.make_client_capabilities()
+-- )
+
 local function on_attach(client, bufnr)
 
-  -- vim.api.nvim_buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+  -- TODO: fix below
+  -- vim.api.nvim_buf_set_option("omnifunc", "v:vim.lsp.omnifunc", ??)
 
-  -- formatting
+  -- Formatting: register autocommand to format LSP attached buffers on save
   if client.server_capabilities.documentFormattingProvider then
     create_autocmd("BufWritePre", {
       group = create_augroup("LspFormatting", { clear = true }),
@@ -20,12 +40,7 @@ local function on_attach(client, bufnr)
   end
 end
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
-)
-
 return {
   on_attach = on_attach,
-  lsp = lsp,
   capabilities = capabilities,
 }
