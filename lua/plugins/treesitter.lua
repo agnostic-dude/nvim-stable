a9c9951d7a29cb7930825217ed1f9b7689299caf
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-12-08 18:43:17 +0530

    Disabled indent.enable option, added parsers for Lua, Python, C & Rust to install automatically and synchronously

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,15 +4,21 @@
 --=========================================================================
 vim.o.foldmethod = "expr"
 vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-vim.o.foldenable = false
+vim.o.foldenable = false -- enable to fold on opening
 
 require("nvim-treesitter.configs").setup({
+  -- Installed parsers: "all" or a list
+  ensure_installed = { "lua", "python", "c", "rust" },
+
+  -- Install parsers synchronously
+  sync_install = true,
+
+  -- When entering a buffer automatically install missing parsers
+  auto_install = true,
+
   highlight = {
     enable = true, -- Core of Treesitter
 
     additional_vim_regex_highlighting = false,
   },
-  indent = {
-    enable = true,
-  }
 })
