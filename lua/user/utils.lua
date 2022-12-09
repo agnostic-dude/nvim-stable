99fe4819d6bced5f4dbcb4edc0f523e535dcb7a1
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-12-10 00:18:55 +0530

    Added utility function to compare 2 tables

--- lua/user/utils.lua
+++ lua/user/utils.lua
@@ -17,3 +17,33 @@ end
 
 _G.create_autocmd = vim.api.nvim_create_autocmd
 _G.create_augroup = vim.api.nvim_create_augroup
+
+-- Get the number of keys in tbl
+local function NumOfKeys(tbl)
+  local n = 0
+  for _, _ in pairs(tbl) do
+    n = n + 1
+  end
+  return n
+end
+
+-- Compare two tables and check whether they have same keys & values
+-- NOTE: if value(s) are tables themselves, search recursively
+function _G.CmpTables(t1, t2)
+  if type(t1) ~= type(t2) then
+    return false
+  end
+  if NumOfKeys(t1) ~= NumOfKeys(t2) then
+    return false
+  end
+  for k, v in pairs(t1) do
+    if type(v) == "table" then
+      if not CmpTables(v, t2[k]) then
+        return false
+      end
+    elseif v ~= t2[k] then
+        return false
+    end
+  end
+  return true
+end
