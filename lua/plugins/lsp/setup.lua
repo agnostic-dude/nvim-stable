c9b1dee042a01c6329a1180690ab728a2f8f0a8e
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-12-10 09:50:51 +0530

    Reconfigured lsp setup

--- lua/plugins/lsp/setup.lua
+++ lua/plugins/lsp/setup.lua
@@ -5,11 +5,14 @@
 
 local lsp_ok, lspconfig = pcall(require, "lspconfig")
 if not lsp_ok then
-  error("Cannot proceed: lspconfig not installed", 2) -- throw back to caller
+  vim.notify("Cannot proceed: lspconfig not installed", vim.log.levels.INFO)
+  return
 end
 
 -- Get compilation of default capabilities & ones provided by nvim-cmp
-local function get_capabilities()
+-- cmp_nvim_lsp has more facilities for textDocument.completion; these are
+-- added to defaults
+local function Get_Capabilities()
   local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   if cmp_nvim_lsp_ok then
@@ -19,49 +22,73 @@ local function get_capabilities()
   return capabilities
 end
 
--- options shared between ALL language servers
-local lsp_defaults = {
-  flags = {
-    -- number of miliseconds to wait before sending next "Update Document"
-    -- Document" notification to language server (default = 150ms)
-    debounce_text_changes = 250,
-  },
-  -- this data is sent to language server to announce what features editor
-  -- can support
-  capabilities = get_capabilities(),
-
-  -- callback function to execute when language server is attached to a buffer
-  on_attach = function(client, bufnr)
-    vim.api.nvim_exec_autocmds("User", { pattern = "LspAttached" })
-    print(client.name .. " attached to buffer " .. bufnr)
-  end,
+local Flags = {
+  -- num of msec to wait before sending "UpdateDocument" request to server
+  debounce_text_changes = 200, -- default = 150
 }
+
+-- callback function to execute when language server is attached to a buffer
+local function On_Attach(client, bufnr)
+  vim.api.nvim_exec_autocmds("User", { pattern = "LspAttached" })
+  print(client.name .. " attached to buffer " .. bufnr)
+end
 
 -- additional settings for INDIVIDUAL language servers
-local lsp_settings = {
-    sumneko_lua = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim" },
-            } },
+local Settings = {
+  sumneko_lua = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" }, -- Get LSP to recognize "vim" global
+      },
+      workspace = {
+        -- make server aware of neovim runtime files
+        library = vim.api.nvim_get_runtime_file(".", true),
+        checkThirdParty = false,
+      },
+      runtime = {
+        version = "LuaJIT", -- Tell LSP which Lua version you're using
+        path = vim.o.runtimepath,
+      }
     },
-    ["rust-analyzer"] = {
-        imports = {
-            granularity = {
-                group = "module",
-            },
-            prefix = "self",
-        },
-        cargo = {
-            buildScripts = {
-                enable = true,
-            },
-        },
-        procMacro = {
-            enable = true,
-        },
-        checkOnSave = {
-            command = "clippy",
-        },
+  },
+  ["rust-analyzer"] = {
+    imports = {
+      granularity = {
+        group = "module",
+      },
+      prefix = "self",
     },
+    cargo = {
+      buildScripts = {
+        enable = true,
+      },
+    },
+    procMacro = {
+      enable = true,
+    },
+    checkOnSave = {
+      command = "clippy",
+    },
+  },
 }
+
+local Servers = {
+  "sumneko_lua",
+  "tsserver",
+  "pyright",
+  "clangd", -- attaching null-ls gives problems with encoding?
+  -- [["rust-analyzer"]],
+}
+
+for _, server in ipairs(Servers) do
+  lspconfig[server].setup({
+    on_attach = On_Attach,
+
+    -- This data is sent to language server to announce what features editor
+    -- can support
+    capabilities = Get_Capabilities(),
+
+    flags = Flags,
+    settings = Settings[server],
+  })
+end
