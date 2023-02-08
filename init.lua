242f06654720f902613d2e111152a59ec1fb982c
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2023-02-09 00:52:08 +0530

    feat: add config for nvim-web-devicons

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 -- Neovim Stable (version 0.8)
 -- Main configuration
 --=============================================================================
---
+
 require("user.settings")
 require("user.utils")
 require("user.keymaps")
@@ -18,6 +18,7 @@ require("plugins.toggleterm")
 require("plugins.gitsigns")
 require("plugins.colorizer")
 require("plugins.telescope")
+require("plugins.devicons")
 
 -- Setup all LSP related plugins by sourcing lua/plugins/lsp/init.lua
 require("plugins.lsp")
