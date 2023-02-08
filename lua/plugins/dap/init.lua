08e38243ce53aa249c28843e8360674ece6fb2f7
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2023-02-08 15:32:45 +0530

    feat: launch dap-ui triggered by dap events

--- lua/plugins/dap/init.lua
+++ lua/plugins/dap/init.lua
@@ -35,6 +35,17 @@ else
   vim.notify_once("Need to install nvim-dap-ui")
 end
 
+-- use nvim-dap events to open and close windows automatically
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
+
 dap.configurations.go = {
   {
     type = "delve",
