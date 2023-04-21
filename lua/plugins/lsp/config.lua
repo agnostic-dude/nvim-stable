e8f95cb7bbf8c6cea2b16f761fac734ca6e4ef36
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2023-04-21 19:18:41 +0530

    refactor: upgrade file structure for LSP configuration to include newly created server_config

---
+++ lua/plugins/lsp/config.lua
@@ -0,0 +1,78 @@
+--=============================================================================
+-- Lsp Configuration
+--=============================================================================
+
+-------------------------------------------------------------------------------
+--                  LSP Server Capabilities
+-------------------------------------------------------------------------------
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+local cmp_nvim_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if cmp_nvim_ok then
+  -- add extra functionality if available
+  capabilities = vim.tbl_deep_extend("force",
+    capabilities, cmp_nvim_lsp.default_capabilities())
+else
+  vim.notify("cmp_nvim_lsp was not found", WARN, { title = "Lspconfig" })
+end
+
+capabilities.offsetEncoding = { "utf-16" } -- clangd uses utf-8 by default
+
+-------------------------------------------------------------------------------
+--                  LSP Server On Attach Callback
+-------------------------------------------------------------------------------
+local set_keymappings = require("plugins.lsp.keymap")
+
+local function on_attach(client, bufnr)
+  set_keymappings(client, bufnr)
+
+  -- vim.api.nvim_buf_create_user_command(bufnr,
+  --   "Format", vim.lsp.buf.format,
+  --   { desc = "Format current buffer with LSP" }
+  -- )
+end
+
+
+-------------------------------------------------------------------------------
+--              Customize LSP Diagnostic Icons
+-------------------------------------------------------------------------------
+local function change_diagnostic_sign(opts)
+  vim.fn.sign_define(opts.name, {
+    text = opts.text,
+    texthl = opts.name,
+    numhl = "",
+  })
+end
+
+local diagnostic_signs = {
+  error   = { name = "DiagnosticSignError", text = "🕱 " },
+  hint    = { name = "DiagnosticSignHint",  text = "" }, -- 💡
+  info    = { name = "DiagnosticSignInfo",  text = "ⓘ " },
+  warning = { name = "DiagnosticSignWarn",  text = "⚠" },
+}
+
+for _, opts in pairs(diagnostic_signs) do
+  change_diagnostic_sign(opts)
+end
+
+-------------------------------------------------------------------------------
+--              Customize LSP Diagnostic Messages
+-------------------------------------------------------------------------------
+vim.diagnostic.config({
+  virtual_text = false,
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = "always",
+  }
+})
+
+
+-- neodev.nvim
+-- fidget.nvim
+
+
+return {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,5 +1,47 @@
---============================================================================
+--=============================================================================
 -- LSP configuration
--- This is the only config sourced out of the files in this dir
---============================================================================
-require("plugins.lsp.setup")
+-- DONE: This file is supposed to collect all server configs and setup the LSP
+-- servers.
+-- Server configs live in `server_config` directory. Requiring this will get a
+-- list of config tables, each table having settings for one language server.
+--=============================================================================
+
+local user_config = require("plugins.lsp.config")
+local server_configs = require("plugins.lsp.server_config")
+
+local mason_installed, mason = pcall(require, "mason")
+local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+if mason_installed then
+  mason.setup()
+  if mason_lspconfig_ok then
+    mason_lspconfig.setup({
+      ensure_installed = vim.tbl_keys(server_configs)
+    })
+  end
+end
+
+local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
+if lspconfig_ok then
+  if mason_lspconfig_ok then
+    mason_lspconfig.setup_handlers({
+      function(server)
+        lspconfig[server].setup(
+          vim.tbl_extend("error", server_configs[server], user_config)
+        )
+      end
+    })
+  else
+  for server, config in pairs(server_configs) do
+    lspconfig[server].setup(vim.tbl_extend("error", config, user_config))
+  end
+  end
+else
+  vim.notify("lspconfig not installed!", ERROR, { title = "Lspconfig" })
+  return 1
+end
+
+-- Setup autocompletion
+require("plugins.lsp.cmp")
+
+-- Setup null-ls server
+require("plugins.lsp.null")
