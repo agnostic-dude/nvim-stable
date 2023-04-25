dfdf9c6b6c5c7bf921bf9bce4bb6d8c1be62da2c
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2023-04-25 18:41:05 +0530

    refactor: variable name changes

--- lua/plugins/lsp/server_config/init.lua
+++ lua/plugins/lsp/server_config/init.lua
@@ -5,11 +5,11 @@
 -- grouped according to the language.  This file sources all of them and creates
 -- a table of server:configuration mappings.
 local server_configs = {}
-local cur_dir = vim.fn.stdpath("config") .. "/lua/plugins/lsp/server_config"
-local configs = vim.fn.globpath(cur_dir, "*.lua")
+local parent_dir = vim.fn.stdpath("config") .. "/lua/plugins/lsp/server_config"
+local configs = vim.fn.globpath(parent_dir, "*.lua", false, true)
 local modules = vim.tbl_map(
   function(o) return vim.fn.fnamemodify(o, ":t:r") end,
-  vim.fn.split(configs)
+  configs
 )
 
 for _, config in ipairs(vim.tbl_filter(function(s) return s ~= "init" end, modules)) do
