b3b222959875aeaaf7e641a813864277daf4aa1b
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2023-05-16 12:05:17 +0530

    refactor: use create_autocmd alias instead of vim.api.nvim_create_autocmd
    I created an alias to vim.api.nvim_create_autocmd in utils.lua to reduce the amount of typing. We can use this.

--- lua/plugins/lsp/config.lua
+++ lua/plugins/lsp/config.lua
@@ -35,7 +35,7 @@ local function on_attach(client, bufnr)
   if client.server_capabilities.documentFormattingProvider then
     local format_on_save = vim.api.nvim_create_augroup("FormatOnSave",
       { clear = true })
-    vim.api.nvim_create_autocmd("BufWritePre", {
+    create_autocmd("BufWritePre", {
       group = format_on_save,
       buffer = bufnr,
       callback = function(ev)
