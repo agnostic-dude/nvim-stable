8857c66efa8e9b28670cc409528961280ad26966
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2023-05-16 12:02:05 +0530

    chore: rename variable null_sources as sources

--- lua/plugins/lsp/null.lua
+++ lua/plugins/lsp/null.lua
@@ -10,7 +10,7 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
-local null_sources = {
+local sources = {
 
   -- Python
   formatting.black.with({ max_line_length = "80" }),
@@ -72,6 +72,6 @@ local lspconfig = require("plugins.lsp.config")
 null_ls.setup({
   autostart = true,
   debug = true,
-  sources = null_sources,
+  sources = sources,
   on_attach = lspconfig.on_attach,
 })
