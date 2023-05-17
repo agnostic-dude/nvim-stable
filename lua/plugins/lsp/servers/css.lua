eca4fbdbab126c8645b2f48374060c50800bb071
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2023-05-17 08:57:25 +0530

    fix: change CSS language server from tailwindcss to cssls
    tailwindcss-language-server appears resource extensive. So, it was uninstalled and replaced with vscode-css-language-server.
    note: vscode-css-language-server (cssls) does not have code formatting capability. But this is Ok, because null-ls does code
    formatting via prettier.

--- lua/plugins/lsp/servers/css.lua
+++ lua/plugins/lsp/servers/css.lua
@@ -3,11 +3,10 @@
 -------------------------------------------------------------------------------
 local servers = {}
 
-if vim.fn.executable("tailwindcss-language-server") then
-  servers.tailwindcss = {
+if vim.fn.executable("vscode-css-language-server") then
+  servers.cssls = {
     -- empty config table
   }
 end
-
 
 return servers
